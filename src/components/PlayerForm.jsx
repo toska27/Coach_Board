905c81ed261ac781511ef220ccef0b35061f2353
toska27 <nemanjatoskic58@gmail.com>
2025-04-10 19:35:28 +0200

    form saved

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -1,25 +1,73 @@
+import { useContext, useState } from "react";
+import PlayerContext from "../context/PlayerContext";
+
 export default function PlayerForm() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [number, setNumber] = useState("");
+  const [position, setPosition] = useState("");
+
+  const { addPlayer } = useContext(PlayerContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newPlayer = {
+      id: number,
+      name,
+      surname,
+      number,
+      position,
+    };
+
+    console.log(newPlayer);
+    addPlayer(newPlayer);
+
+    setName("");
+    setSurname("");
+    setNumber("");
+    setPosition("");
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="surname">Surname:</label>
-          <input type="text" name="surname" />
+          <input
+            type="text"
+            name="surname"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="number">Jersey number:</label>
-          <input type="number" name="number" />
+          <input
+            type="number"
+            name="number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="position">Position:</label>
-          <select>
+          <select
+            value={position}
+            onChange={(e) => setPosition(e.target.value)}
+          >
             <option value="pg">PG</option>
             <option value="sg">SG</option>
             <option value="sf">SF</option>
