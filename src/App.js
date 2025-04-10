9f0372622a336d93586d07acd24807d8a20f1b4f
toska27 <nemanjatoskic58@gmail.com>
2025-04-10 19:35:12 +0200

    added context

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ import MatchForm from "./components/MatchForm";
 import PerformanceForm from "./components/PerformanceForm";
 import PerformanceTable from "./components/PerformanceTable";
 import PlayerStats from "./components/PlayerStats";
+import { PlayerProvider } from "./context/PlayerContext";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<PlayerForm />} />
-        <Route path="/add-match" element={<MatchForm />} />
-        <Route path="/add-performance" element={<PerformanceForm />} />
-        <Route path="/perfomaces" element={<PerformanceTable />} />
-        <Route path="/player_stats" element={<PlayerStats />} />
-      </Routes>
+      <div className="app">
+        <PlayerProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<PlayerForm />} />
+            <Route path="/add-match" element={<MatchForm />} />
+            <Route path="/add-performance" element={<PerformanceForm />} />
+            <Route path="/perfomaces" element={<PerformanceTable />} />
+            <Route path="/player_stats" element={<PlayerStats />} />
+          </Routes>
+        </PlayerProvider>
+      </div>
     </Router>
   );
 }
