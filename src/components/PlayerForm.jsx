75690e04ee4744516cf0d586c1fd0b16abf0c0fe
toska27 <nemanjatoskic58@gmail.com>
2025-04-13 16:08:51 +0200

    form validation

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -2,14 +2,53 @@ import { useContext, useState } from "react";
 import CoachContext from "../context/CoachContext";
 
 export default function PlayerForm() {
-  const [name, setName] = useState("");
+  const [nameValue, setNameValue] = useState("");
   const [number, setNumber] = useState("");
   const [position, setPosition] = useState("");
+  const [errors, setErorrs] = useState({
+    nameError: "",
+    numberError: "",
+    positionError: "",
+  });
 
   const { addPlayer } = useContext(CoachContext);
 
+  let name = nameValue.trim();
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+
+  function validation() {
+    let formErrors = {};
+
+    // Name Validation
+    if (name === "") {
+      formErrors.nameError = "Name is required";
+    } else if (name.length < 2) {
+      formErrors.nameError = "Name must be at least 2 characters.";
+    } else if (!/^[A-Za-z]+$/.test(name)) {
+      formErrors.nameError = "Name can only contain letters.";
+    }
+
+    // Number validation
+    if (number < 0 || number > 99 || number === "") {
+      formErrors.numberError = "You must enter number beetwen 0 and 99";
+    }
+
+    // Position validation
+    if (position === "Select position:" || position === "") {
+      formErrors.positionError = "Select poistion of player.";
+    }
+
+    setErorrs(formErrors);
+    return Object.keys(formErrors).length === 0; // valid if no errors
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const isValid = validation();
+    if (!isValid) {
+      return;
+    }
 
     const newPlayer = {
       id: Date.now(),
@@ -20,9 +59,12 @@ export default function PlayerForm() {
 
     addPlayer(newPlayer);
 
-    setName("");
+    console.log(newPlayer);
+
+    setNameValue("");
     setNumber("");
     setPosition("");
+    setErorrs({});
   }
 
   return (
@@ -33,9 +75,10 @@ export default function PlayerForm() {
             type="text"
             name="player"
             placeholder="Name:"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={nameValue}
+            onChange={(e) => setNameValue(e.target.value)}
           />
+          {errors.nameError && <p>{errors.nameError}</p>}
         </div>
 
         <div>
@@ -46,6 +89,7 @@ export default function PlayerForm() {
             value={number}
             onChange={(e) => setNumber(e.target.value)}
           />
+          {errors.numberError && <p>{errors.numberError}</p>}
         </div>
 
         <div>
@@ -60,6 +104,7 @@ export default function PlayerForm() {
             <option value="pf">PF</option>
             <option value="c">C</option>
           </select>
+          {errors.positionError && <p>{errors.positionError}</p>}
         </div>
 
         <button type="submit">Submit</button>
